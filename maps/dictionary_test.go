58c657cfb4dca9eab7f535ca5aad40b44ecfe570
Bjarne <83133360+BjarneVerschorre@users.noreply.github.com>
2024-03-24 18:06:35 +0100

    Added Delete func

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -8,7 +8,7 @@ func TestSearch(t *testing.T) {
 	t.Run("known word", func(t *testing.T) {
 		got, _ := dictionary.Search("test")
 		want := "this is just a test"
-	
+
 		assertStrings(t, got, want)
 	})
 
@@ -53,25 +53,36 @@ func TestUpdate(t *testing.T) {
 		definition := "this is just a test"
 		dictionary := Dictionary{word: definition}
 		newDefinition := "new definition"
-	
+
 		err := dictionary.Update(word, newDefinition)
-	
+
 		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, newDefinition)
 	})
-	
+
 	t.Run("new word", func(t *testing.T) {
 		word := "test"
 		definition := "this is just a test"
 		dictionary := Dictionary{}
-	
+
 		err := dictionary.Update(word, definition)
-	
+
 		assertError(t, err, ErrWordDoesNotExist)
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
 
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
+
+}
 
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
